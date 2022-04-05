8834bc06ba4e0edcf1522e8c5d8bfcf872c0c113
Smartico <75529844+SmarticoApps@users.noreply.github.com>
2022-04-05 15:50:32 -0500

    blog first commit

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.end("hello world!!!!!!!!!!!");
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   res.end("hello world!");
+// });
+// server.listen(80);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require('dotenv').config()
+
+const app = express();
+
+// parse application/json
+app.use(bodyParser.json())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var corsOptions = {
+  origin: 'http://localhost:3000'
+}
+// use cors options
+app.use(cors(corsOptions))
+
+// 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+
+// routes
+const blog = require('./app/routes/blog')
+app.use('/api/blog', blog)
+
+// listening port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
-server.listen(80);
